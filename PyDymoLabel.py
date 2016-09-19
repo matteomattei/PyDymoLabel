8b9496908537fb9e048ee2db565eef508db39970
Matteo Mattei <matteo.mattei@gmail.com>
2016-09-19 12:59:27 +0200

    remove msgbox and add tk UI

--- PyDymoLabel.py
+++ PyDymoLabel.py
@@ -5,7 +5,48 @@ from os import path
 from datetime import datetime, timedelta
 from win32com.client import Dispatch
 from tkinter import Tk
-import tkinter.messagebox as mbox
+from tkinter import Label
+from tkinter import Button
+from tkinter import Entry
+
+def center(win):
+	win.update_idletasks()
+	width = win.winfo_width()
+	height = win.winfo_height()
+	x = (win.winfo_screenwidth() // 2) - (width // 2)
+	y = (win.winfo_screenheight() // 2) - (height // 2)
+	win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+def printLabels():
+	num_labels = e.get()
+	if not num_labels.isnumeric():
+		labelmsg.config(text="Plese enter a number!")
+		return
+	labelmsg.config(text="")
+	e.configure(state='disabled')
+	try:
+		now = datetime.now()
+		next = now + timedelta(30)
+
+		labelCom = Dispatch('Dymo.DymoAddIn')
+		labelText = Dispatch('Dymo.DymoLabels')
+		isOpen = labelCom.Open(mylabel)
+		selectPrinter = 'DYMO LabelWriter 450'
+		labelCom.SelectPrinter(selectPrinter)
+
+		labelText.SetField('TESTO2', now.strftime('%Y/%m/%d'))
+		labelText.SetField('TESTO4', next.strftime('%Y/%m/%d'))
+
+		labelCom.StartPrintJob()
+		labelCom.Print(num_labels,False)
+		labelCom.EndPrintJob()
+
+		labelmsg.configure(text='Labels printed!')
+	except:
+		labelmsg.configure(text='An error occurred during printing.')
+
+	e.configure(state='normal')
+	submit.configure(state='normal')
 
 curdir = None
 if getattr(sys, 'frozen', False):
@@ -16,32 +57,31 @@ else:
 	curdir = path.dirname(path.abspath(__file__))
 
 mylabel = path.join(curdir,'my.label')
-window = Tk()
-window.wm_withdraw()
+root = Tk()
+root.geometry('280x100')
+root.resizable(width=False, height=False)
+root.title('PyDymoLabel by Chip2Bit')
+center(root)
+
+label = Label(root, text="How many labels to print?")
+e = Entry(root, justify='right')
+e.focus_set()
+e.insert(0,'1') # 1 is default
+labelmsg = Label(root, text="")
+
+num_labels = 0
 
 if not path.isfile(mylabel):
-	mbox.showinfo('PyDymoLabel','Template file my.label does not exist')
-	sys.exit(1)
+	labelmsg.Label(root,text='Template file my.label does not exist')
+	e.configure(state='disabled')
+	submit.configure(state='disabled')
 
-try:
-	now = datetime.now()
-	next = now + timedelta(30)
+submit = Button(root, text ="Print", command = printLabels)
 
-	labelCom = Dispatch('Dymo.DymoAddIn')
-	labelText = Dispatch('Dymo.DymoLabels')
-	isOpen = labelCom.Open(mylabel)
-	selectPrinter = 'DYMO LabelWriter 450'
-	labelCom.SelectPrinter(selectPrinter)
+label.pack()
+e.pack()
+labelmsg.pack()
+submit.pack()
 
-	labelText.SetField('TESTO2', now.strftime('%Y/%m/%d'))
-	labelText.SetField('TESTO4', next.strftime('%Y/%m/%d'))
+root.mainloop()
 
-	labelCom.StartPrintJob()
-	labelCom.Print(1,False)
-	labelCom.EndPrintJob()
-except:
-	mbox.showinfo('PyDymoLabel','An error occurred during printing.')
-	sys.exit(1)
-
-mbox.showinfo('PyDymoLabel','Label printed!')
-sys.exit(0)
--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@ rmdir build /s /q
 rmdir PYDYMOLABEL-Portable /s /q
 del /s /q PyDymoLabel.exe
 C:\Python34\python.exe setup.py build
-"C:\Program Files\NSIS\makensis.exe" compile.nsi
+"C:\Program Files (x86)\NSIS\makensis.exe" compile.nsi
+pause
